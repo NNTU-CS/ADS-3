2dd06b14326f84c29865544bdaf479db06ddd979
deniskr12 <kremnden@gmail.com>
2024-03-10 15:11:12 +0300

    ADS-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    uint64_t count = 0;
+    for (uint64_t i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            count++;
+        }
+    }
+    return count;
 }
