32518af155cdccb92ebc197e5db164d828fabecd
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-03-15 15:36:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int left = 0;
+    int right = size - 1;
+    int r;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            r++;
+            int i = mid + 1;
+            while (i < size && arr[i] == value) {
+                r+=1;
+                i++;
+            }
+            i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
+                r+=1;
+                i--;
+            }
+            return r;
+        } 
+        else if (arr[mid] >= value) {
+            right = mid - 1;
+        } 
+        else {
+            left = mid + 1;
+        }
+    }
+    return 0; // если ничего не найдено
 }
