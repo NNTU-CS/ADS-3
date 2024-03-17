24ef7052b7ab08189ef35a7201748bb6a313907e
flapik19 <selivanovvlad3211@gmail.com>
2024-03-17 21:26:27 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int count = 0; 
-  for (int i = 0; i < size; i++){
-    if (arr[i] == value)
-      count++;
-    }
+int count = 0;
+for (int i = 0; i < size; i++) {
+  if (arr[i] == value)
+    count++;
+  }
   if (count != 0)
     return count;
   else
     return 0;
-
 }
