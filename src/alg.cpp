184a5de1c82c3a5206b8d8860238aaa4aa1d67d1
flapik19 <selivanovvlad3211@gmail.com>
2024-03-19 20:47:33 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,24 @@
 
 int cbinsearch(int *arr, int size, int value) {
 int count = 0;
-for (int i = 0; i < size; i++) {
-  if (arr[i] == value)
-    count++;
+  int left = 0;
+  int right = size - 1;
+  for (int i = 0; i < size; i++) {
+    int mid = (left + right) / 2;
+    if (arr[mid] > value)
+      left = mid + 1;
+    else if (arr[mid] < value)
+      right = mid - 1;
+    else
+      count++;
+    int k = mid + 1;
+    while(arr[k] == value)
+      count++;
+      i++;
+    int j = mid - 1;
+    while(arr[j] == value)
+      count++;
+      j--;
   }
-  if (count != 0)
-    return count;
-  else
-    return 0;
+  return count;
 }
