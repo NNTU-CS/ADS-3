ecf553b10b7b832f2a79ccf3702e7c32fd907992
Jkalashnikova <kalaschnikowa2005@gmail.com>
2024-03-23 15:25:20 +0300

    ADS-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 int cbinsearch(int *arr, int size, int value) {
-    int64 low = 0;
-    int64 left = 0;
-    int64 right = size - 1;
+    uint_64 low = 0;
+    uint_64 left = 0;
+    uint_64 right = size - 1;
     while (left <= right) {
-        int64 mid = (right + left) / 2;
+        int_64 mid = (right + left) / 2;
         if (arr[mid] == value) {
             low++;
         } else {
