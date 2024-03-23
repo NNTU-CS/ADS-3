6c048b2b078e0d510a201cf5f4a35986b2bfcbaf
VadimKochnev <124500780+sendemy@users.noreply.github.com>
2024-03-23 21:47:37 +0300

    b259

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,35 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+  int right = size - 1;
+  int counter = 0;
+
+  while (left <= right) {
+    int middle = left + (right - left) / 2;
+
+    if (arr[middle] == value) {
+      counter++;
+      int tempMiddle = middle;
+
+      while (tempMiddle > 0 && arr[tempMiddle - 1] == value) {
+        counter++;
+        tempMiddle--;
+      }
+
+      tempMiddle = middle;
+
+      while (tempMiddle < size - 1 && arr[tempMiddle + 1] == value) {
+        counter++;
+        tempMiddle++;
+      }
+
+      return counter;
+    } else if (arr[middle] < value)
+      left = middle + 1;
+    else
+      right = middle - 1;
+  }
+
+  return 0;
 }
