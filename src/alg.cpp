869454ae01e619baeef8e32ae80ba7ad2c82c9c4
flapik19 <selivanovvlad3211@gmail.com>
2024-03-28 19:46:32 +0300

    ads

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,8 +19,7 @@ int cbinsearch(int *arr, int size, int value) {
         i++;
       }
       return count;
-    }
-    else if (arr[mid] < value)
+    } else if (arr[mid] < value)
       left = mid + 1;
     else
       right = mid - 1;
