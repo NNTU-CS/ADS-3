feb035cdb18057125e38f328c7fd5c243863bad8
flapik19 <selivanovvlad3211@gmail.com>
2024-03-28 19:42:24 +0300

    alg

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,30 +1,38 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  int count = 0;
+int cbinsearch(int *arr, int size, int value)
+{
   int left = 0;
   int right = size - 1;
-  while (right >= left) {
+  int count = 0;
+  while (left <= right)
+  {
     int mid = left + (right - left) / 2;
-    if (arr[mid] == value) {
-      int index = mid - 1;
-      while (index >= 0 && arr[index] == value) {
+    if (arr[mid] == value)
+    {
+      count++;
+      int i = mid - 1;
+      while (i >= 0 && arr[i] == value)
+      {
         count++;
-        index--;
+        i--;
       }
-      index = mid + 1;
-      while (index < size && arr[index] == value) {
+      i = mid + 1;
+      while (i < size && arr[i] == value)
+      {
         count++;
-        index++;
+        i++;
       }
       return count;
     }
-    else if (arr[mid] < value) {
+    else if (arr[mid] < value)
+    {
       left = mid + 1;
     }
-    else {
+    else
+    {
       right = mid - 1;
     }
   }
-  return 0;
+  return 0; // если ничего не найдено
 }
