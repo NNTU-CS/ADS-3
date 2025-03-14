c71ccef89c7c2d544f3ed985c7959669f250e48d
Andrey20-07 <mr.klach999@gmail.com>
2025-03-14 21:53:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,7 +22,7 @@ unsigned int collatzLen(uint64_t num) {
   while (num > 1) {
     if (num % 2 == 0) {
       num = num / 2;
-    } else (num % 2 == 1) {
+    } else {
       num = 3 * num + 1;
     }
     result++;
