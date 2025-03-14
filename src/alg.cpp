35385f075a33bf623f4f3652a636be66026fcb0c
Andrey20-07 <mr.klach999@gmail.com>
2025-03-14 21:33:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  uint64_t maximum = 0;
+  uint64_t maximum = num;
   while (num > 1) {
     if (num % 2 == 0) {
       num = num / 2;
@@ -19,7 +19,7 @@ uint64_t collatzMaxValue(uint64_t num) {
 }
 
 unsigned int collatzLen(uint64_t num) {
-  unsigned int result = 0;
+  unsigned int result = 1;
   while (num > 1) {
     if (num % 2 == 0) {
       num = num / 2;
