82c7391397b09138a36772ec6e20a6a1513cdae3
Andrey20-07 <mr.klach999@gmail.com>
2025-03-14 21:47:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,8 +7,7 @@ uint64_t collatzMaxValue(uint64_t num) {
   while (num > 1) {
     if (num % 2 == 0) {
       num = num / 2;
-    }
-    if (num % 2 == 1) {
+    } else {
       num = 3 * num + 1;
     }
     if (num > maximum) {
@@ -23,8 +22,7 @@ unsigned int collatzLen(uint64_t num) {
   while (num > 1) {
     if (num % 2 == 0) {
       num = num / 2;
-    }
-    if (num % 2 == 1) {
+    } else (num % 2 == 1) {
       num = 3 * num + 1;
     }
     result++;
