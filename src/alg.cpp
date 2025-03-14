9f91f6c37e8113ca66fffde281b8659e44923436
KorobovaKate <kat3kor@yandex.ru>
2025-03-14 20:26:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,7 +18,7 @@ uint64_t collatzMaxValue(uint64_t num) {
 }
 
 unsigned int collatzLen(uint64_t num) {
-  unsigned int lenght = 0;
+  unsigned int lenght = 1;
   while (num != 1) {
     if (num % 2 == 0) {
       num = num / 2;
@@ -42,5 +42,6 @@ unsigned int seqCollatz(unsigned int *maxlen,
       numMaxLenght = i;
     }
   }
+  *maxlen = maxLength
   return numMaxLenght;
 }
