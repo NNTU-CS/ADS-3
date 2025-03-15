1732769df4480013b14578950a211ff7767f2cb0
Alexey-Efimov767 <alexefimov565@gmail.com>
2025-03-15 07:56:13 +0300

    Update1.2(3) alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,10 +23,9 @@ unsigned int collatzLen(uint64_t num) {
   int len = 1;
 while (num != 1) {
     if (num % 2 == 1) {
-        num = 3 * num + 1;
-    } 
-    else {
-        num = num / 2;
+      num = 3 * num + 1;
+    } else {
+      num = num / 2;
     }
     len++;
 }
@@ -39,7 +38,7 @@ unsigned int seqCollatz(unsigned int *maxlen,
   uint64_t num = 0;
 unsigned int maxLen = 0;
 for (uint64_t i = lbound; i <= rbound; i++) {
-    if (int(collatzLen(i)) >= int(maxLen)) {
+    if (static_cast<int>(collatzLen(i)) >= static_cast<int>(maxLen)) {
         maxLen = collatzLen(i);
         num = i;
     }
