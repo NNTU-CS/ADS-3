ead33713217e64d9423c6b1e817d918672d08f3f
Alexey-Efimov767 <alexefimov565@gmail.com>
2025-03-15 07:49:33 +0300

    Update1.1(3) alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,49 @@
-// Copyright 2025 NNTU-CS
+//Copyright [2025] <Copyright Alexey Efimov>
 #include <cstdint>
+#include <iostream>
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  uint64_t x = num;
+while (num != 1) {
+    if (num % 2 == 1) {
+        num = 3 * num + 1;
+        if (x <= num) {
+            x = num;
+        }
+    }
+    if (num % 2 == 0) {
+        num = num / 2;
+    }
+}
+return x;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  int len = 1;
+while (num != 1) {
+    if (num % 2 == 1) {
+        num = 3 * num + 1;
+    } 
+    else {
+        num = num / 2;
+    }
+    len++;
+}
+return len;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  uint64_t num = 0;
+unsigned int maxLen = 0;
+for (uint64_t i = lbound; i <= rbound; i++) {
+    if (int(collatzLen(i)) >= int(maxLen)) {
+        maxLen = collatzLen(i);
+        num = i;
+    }
+}
+*maxlen = maxLen;
+return num;
 }
