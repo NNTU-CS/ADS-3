0a8e7f7c55d529aed1d11dbcfef0a27452732ac4
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-03-17 11:30:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,8 +7,7 @@ uint64_t collatzMaxValue(uint64_t num) {
   while (num != 1) {
     if (num % 2 == 0) {
       num /= 2;
-    }
-    else {
+    } else {
       num = 3 * num + 1;
     }
     if (num > maxValue) {
@@ -23,8 +22,7 @@ unsigned int collatzLen(uint64_t num) {
   while (n != 1) {
     if (n % 2 == 0) {
       n /= 2;
-    } 
-    else {
+    } else {
       n = 3 * n + 1;
     }
     len++;
