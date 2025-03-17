b5b4d1002aab9d74e5a36a91d024119dd3135acd
Eternity8c <morozkind6@gmail.com>
2025-03-17 18:26:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,7 +30,9 @@ unsigned int collatzLen(uint64_t num) {
     return col;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+unsigned int seqCollatz(unsigned int *maxlen,
+                        uint64_t lbound,
+                        uint64_t rbound) {
     *maxlen = 0;
     unsigned int maxCol = 0;
     uint64_t resultNum = lbound;
