c09bca404c12fb8f01a08566492b93ff863d4f99
Kirill115fz <kirillkuznecov716@gmail.com>
2025-03-18 12:50:42 +0300

    456

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,35 +3,38 @@
 #include "alg.h"
 
 unsigned int collatzLen(uint64_t num) {
-    unsigned int len = 0;
-    if (num == 0) return 1;
-    while (num > 0) {
-        num /= 10;
-        len++;
-    }
-    return len;
+  unsigned int len = 0;
+  while (num != 1) {
+    if (num % 2 == 1)
+      num = 3 * num + 1;
+    else
+      num /= 2;
+    len++;
+  }
+  return ++len;
 }
 
 uint64_t collatzMaxValue(uint64_t num) {
-    uint64_t sum = 0;
-    while (num > 0) {
-        sum += num % 10;
-        num /= 10;
-    }
-    return sum;
+  uint64_t max = num;
+  while (num != 1) {
+    if (num % 2 == 1)
+      num = 3 * num + 1;
+    else
+      num /= 2;
+    max = max > num ? max : num;
+  }
+  return max;
 }
 
-unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound, uint64_t rbound) {
-    unsigned int num = 0;
-    *maxlen = 0xFFFFFFFF;
-
-    for (uint64_t i = lbound; i <= rbound; ++i) {
-        unsigned int digitSum = static_cast<unsigned int>(collatzMaxValue(i));
-        if (*maxlen > digitSum) {
-            *maxlen = digitSum;
-            num = static_cast<unsigned int>(i);
-        }
+unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound,
+                        uint64_t rbound) {
+  unsigned int num = 0;
+  *maxlen = 0;
+  for (uint64_t i = lbound; i <= rbound; i++) {
+    if (*maxlen < collatzLen(i)) {
+      *maxlen = collatzLen(i);
+      num = i;
     }
-
-    return num;
-}
+  }
+  return num;
+  }
