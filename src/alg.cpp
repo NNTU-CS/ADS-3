067666c90650e05f5f4c154b74c5225ba7472823
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-03-18 20:19:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,20 +3,6 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  unsigned int length = 1;
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
-        } else {
-            num = 3 * num + 1;
-        }
-        length++;
-    }
-    return length;
-
-}
-
-unsigned int collatzLen(uint64_t num) {
   uint64_t maxVal = num;
     while (num != 1) {
         if (num % 2 == 0) {
@@ -31,24 +17,32 @@ unsigned int collatzLen(uint64_t num) {
     return maxVal;
 }
 
+unsigned int collatzLen(uint64_t num) {
+  unsigned int length = 0;
+    while (num != 1) {
+        length++;
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+    }
+    return length + 1;
+}
+
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  unsigned int longestLen = 0;
+  uint64_t lbound, uint64_t rbound) {
+    unsigned int longestLen = 0;
     uint64_t numberWithLongestSeq = 0;
-    uint64_t maxInLongestSeq = 0;
-
     for (uint64_t i = lbound; i <= rbound; i++) {
         unsigned int currentLen = collatzLen(i);
-        uint64_t currentMax = collatzMaxValue(i);
-        
         if (currentLen > longestLen) {
             longestLen = currentLen;
             numberWithLongestSeq = i;
-            maxInLongestSeq = currentMax;
         }
     }
-    
     *maxlen = longestLen;
     return numberWithLongestSeq;
 }
