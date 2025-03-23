f5f3bf8085ec525dbfe063048361616dc9395141
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-03-23 12:29:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,10 +4,10 @@
 
 uint64_t collatzMaxValue(uint64_t num) {
   uint64_t maximum = 0;
-  while (num != 1){
-      if (num % 2 == 0) {num = num/2;}
+  while (num != 1) {
+      if (num % 2 == 0) {num = num / 2;}
       else {num = num * 3 + 1;}
-      if (maximum < num){ maximum = num;}
+      if (maximum < num) {maximum = num;}
     }
   return maximum;
 }
@@ -15,8 +15,12 @@ uint64_t collatzMaxValue(uint64_t num) {
 unsigned int collatzLen(uint64_t num) {
   uint64_t len = 1;
   while (num != 1) {
-    if (num % 2 == 0) { num = num / 2;}
-    else {num = num * 3 + 1;}
+    if (num % 2 == 0) {
+      num = num / 2;
+    }
+    else {
+      num = num * 3 + 1;
+    }
     len += 1;
   }
   return len;
@@ -27,12 +31,11 @@ unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t rbound) {
   unsigned int maximum = 0;
   unsigned int startNum = 0;
-  for (uint64_t i = lbound; i < rbound; i++)
-    {
-      if (collatzLen(i) > maximum) {
-          maximum = collatzLen(i);
-          startNum = i;
-        }
+  for (uint64_t i = lbound; i < rbound; i++) {
+    if (collatzLen(i) > maximum) {
+        maximum = collatzLen(i);
+        startNum = i;
+      }
     }
     *maxlen = maximum;
     return startNum;
