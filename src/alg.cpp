28a0d38f2d56eb7dd7707b13d0563a8e52cd2ec5
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-03-23 12:34:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,8 +7,7 @@ uint64_t collatzMaxValue(uint64_t num) {
   while (num != 1) {
       if (num % 2 == 0) {
         num = num / 2;
-      }
-      else {
+      } else {
         num = num * 3 + 1;
       }
       if (maximum < num) {
@@ -23,8 +22,7 @@ unsigned int collatzLen(uint64_t num) {
   while (num != 1) {
     if (num % 2 == 0) {
       num /= 2;
-    }
-    else {
+    } else {
       num = num * 3 + 1;
     }
     len += 1;
