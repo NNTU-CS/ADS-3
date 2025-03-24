885b83738cf32eaf1f3d00fddd3e984cb6ec1333
TechnoMagicc <leyzarof@gmail.com>
2025-03-24 12:47:09 +0300

    v1.4

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,7 +18,7 @@ uint64_t collatzMaxValue(uint64_t num) {
 }
 unsigned int collatzLen(uint64_t num) {
   unsigned int len = 1;
-    while (num != 1){
+    while (num != 1) {
       if (num % 2 == 0) {
           num /= 2;
       } else {
