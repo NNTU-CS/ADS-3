8f7efd11f92239be214c95e4534fa0b6656194c8
TechnoMagicc <leyzarof@gmail.com>
2025-03-24 12:38:46 +0300

    v1.2

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,7 +16,6 @@ uint64_t collatzMaxValue(uint64_t num) {
     }
     return maxValue;
 }
-
 unsigned int collatzLen(uint64_t num) {
   unsigned int len = 1;
   while (num != 1){
@@ -30,7 +29,6 @@ unsigned int collatzLen(uint64_t num) {
   } 
   return len;
 }
-
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
