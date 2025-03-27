78bbdae2fea8da10b56c23206a3dc4e3f83f7908
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-03-27 23:17:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,8 @@
 // Copyright 2025 NNTU-CS
 #include <cstdint>
-
+#include <algorithm>
 #include "alg.h"
-
-using namespace std;
+#include <iostream>
 
 unsigned int collatzLen(uint64_t num) {
   unsigned int len = 1;
@@ -26,29 +25,26 @@ uint64_t collatzMaxValue(uint64_t num) {
     } else {
       num = 3 * num + 1;
     }
-    max_val = max(max_val, num);
+    if (num > max_val) {
+      max_val = num;
+    }
   }
   return max_val;
 }
 
-unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound,
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound,
                         uint64_t rbound) {
-  if (lbound > rbound) {
-    *maxlen = 0;
-    return 0;
-  }
-
-  unsigned int max_len = 0;
-  unsigned int best_num = 0;
+  unsigned int max_length = 0;
+  unsigned int result = 0;
 
   for (uint64_t i = lbound; i <= rbound; ++i) {
-    unsigned int current_len = collatzLen(i);
-    if (current_len > max_len) {
-      max_len = current_len;
-      best_num = static_cast<unsigned int>(i);
+    unsigned int current_length = collatzLen(i);
+    if (current_length > max_length) {
+      max_length = current_length;
+      result = static_cast<unsigned int>(i);
     }
   }
 
-  *maxlen = max_len;
-  return best_num;
+  *maxlen = max_length;
+  return result;
 }
