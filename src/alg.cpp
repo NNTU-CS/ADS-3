4eebc91d8a7d5f007f5e4090afcd52a99ca3948d
Burkov06 <s.burkov.2006@gmail.com>
2025-03-30 21:14:35 +0300

    ADS3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,41 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  if (num == 1) return 1;
+    if (collatz_cache.count(num)) return collatz_cache[num];
+    
+    unsigned int length = 1 + collatzLen(num % 2 == 0 ? num / 2 : 3 * num + 1);
+    collatz_cache[num] = length;
+    return length;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  uint64_t maxVal = num;
+    while (num != 1) {
+        if (num % 2 == 0)
+            num /= 2;
+        else
+            num = 3 * num + 1;
+        
+        if (num > maxVal)
+            maxVal = num;
+    }
+    return maxVal;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+   unsigned int longestNum = 0;
+    *maxlen = 0;
+    
+    for (uint64_t i = lbound; i <= rbound; i++) {
+        unsigned int length = collatzLen(i);
+        if (length > *maxlen) {
+            *maxlen = length;
+            longestNum = i;
+        }
+    }
+    
+    return longestNum;
 }
