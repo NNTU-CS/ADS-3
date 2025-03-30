6d692626f9c04910ff356e344ff2351188157d78
Burkov06 <s.burkov.2006@gmail.com>
2025-03-30 21:28:05 +0300

    ADS3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,34 +3,34 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  if (num == 1) return 1;
-    if (collatz_cache.count(num)) return collatz_cache[num];
-    
-    unsigned int length = 1 + collatzLen(num % 2 == 0 ? num / 2 : 3 * num + 1);
-    collatz_cache[num] = length;
-    return length;
-}
-
-unsigned int collatzLen(uint64_t num) {
-  uint64_t maxVal = num;
+    uint64_t maxVal = num;
     while (num != 1) {
-        if (num % 2 == 0)
+        if (num % 2 == 0) {
             num /= 2;
-        else
+        } else {
             num = 3 * num + 1;
-        
-        if (num > maxVal)
+        }
+
+        if (num > maxVal) {
             maxVal = num;
+        }
     }
     return maxVal;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-   unsigned int longestNum = 0;
+unsigned int collatzLen(uint64_t num) {
+    if (num == 1) {
+        return 1;
+    }
+
+    unsigned int length = 1 + collatzLen(num % 2 == 0 ? num / 2 : 3 * num + 1);
+    return length;
+}
+
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int longestNum = 0;
     *maxlen = 0;
-    
+
     for (uint64_t i = lbound; i <= rbound; i++) {
         unsigned int length = collatzLen(i);
         if (length > *maxlen) {
@@ -38,6 +38,6 @@ unsigned int seqCollatz(unsigned int *maxlen,
             longestNum = i;
         }
     }
-    
+
     return longestNum;
 }
