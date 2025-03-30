e4f39e6f164b9516ce132d1e12c394ea9140e4c6
nastuy <nastuysandalova@gmail.com>
2025-03-30 20:48:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,8 +9,7 @@ uint64_t collatzMaxValue(uint64_t num) {
       num = num / 2;
       if (max < num)
         max = num;
-    }
-    else {
+    } else {
       num = 3 * num + 1;
       if (max < num)
         max = num;
@@ -20,13 +19,12 @@ uint64_t collatzMaxValue(uint64_t num) {
 }
 
 unsigned int collatzLen(uint64_t num) {
-  int res = 1;
+  unsignet int res = 1;
   while (num != 1) {
     if (num % 2 == 0) {
       num = num / 2;
       ++res;
-    }
-    else {
+    } else {
       num = 3 * num + 1;
       ++res;
     }
@@ -37,8 +35,8 @@ unsigned int collatzLen(uint64_t num) {
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound) {
-  uint64_t num = lbound;
-  uint64_t len;
+  unsigned int num = lbound;
+  unsigned int len;
   for (int i = lbound; i <= rbound; ++i) {
     len = collatzLen(i);
     if (len > maxlen) {
