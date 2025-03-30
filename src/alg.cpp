821e8e5560caa0612121571af30bbb3b034601a5
yeefip <yeefipp@gmail.com>
2025-03-30 14:18:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,39 @@
-// Copyright 2025 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<uint64_t, unsigned int> collatz_cache;
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+    uint64_t max_value = num;
+    while (num != 1) {
+        num = (num % 2 == 0) ? num / 2 : 3 * num + 1;
+        if (num > max_value) max_value = num;
+    }
+    return max_value;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    if (num == 1) return 1;
+    if (collatz_cache.count(num)) return collatz_cache[num];
+    
+    unsigned int length = 1 + collatzLen(num % 2 == 0 ? num / 2 : 3 * num + 1);
+    collatz_cache[num] = length;
+    return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int longest_num = 0;
+    *maxlen = 0;
+    
+    for (uint64_t num = lbound; num <= rbound; ++num) {
+        unsigned int length = collatzLen(num);
+        if (length > *maxlen) {
+            *maxlen = length;
+            longest_num = num;
+        }
+    }
+    return longest_num;
 }
