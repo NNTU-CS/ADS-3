1982c709fb1291a235eaa4e9e1bf70bcdac2a2d1
jafar khadour <jkhadour111@gmail.com>
2025-03-30 12:55:19 +0300

    alg21.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 
 uint64_t collatzMaxValue(uint64_t num) {
   uint64_t max = num;
-  
+
   while (num != 1) {
     if (num % 2 == 0) {
       num /= 2;
@@ -15,13 +15,13 @@ uint64_t collatzMaxValue(uint64_t num) {
       max = num;
     }
   }
-  
+
   return max;
 }
 
 unsigned int collatzLen(uint64_t num) {
   unsigned int len = 1;
-  
+
   while (num != 1) {
     if (num % 2 == 0) {
       num /= 2;
@@ -30,7 +30,7 @@ unsigned int collatzLen(uint64_t num) {
     }
     len++;
   }
-  
+
   return len;
 }
 
@@ -39,7 +39,7 @@ unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t rbound) {
   unsigned int max_len = 0;
   unsigned int res_num = 0;
-  
+
   for (uint64_t i = lbound; i <= rbound; i++) {
     unsigned int current_len = collatzLen(i);
     if (current_len > max_len) {
@@ -47,7 +47,7 @@ unsigned int seqCollatz(unsigned int *maxlen,
       res_num = i;
     }
   }
-  
+
   *maxlen = max_len;
   return res_num;
 }
