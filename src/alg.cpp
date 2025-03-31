07dde1ef69559a82e72f4fadb691e364fa4be576
foltukol <foltukol@yandex.ru>
2025-03-31 23:56:35 +0300

    ADS-3 | b51 | Kurmaev Andrey

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ uint64_t collatzMaxValue(uint64_t num) {
     if (num % 2 == 1) {
       num = num * 3 + 1;
       max_val = (max_val > num ? max_val : num);
-    } 
+    }
     else {
       num /= 2;
     }
@@ -22,7 +22,7 @@ unsigned int collatzLen(uint64_t num) {
     len++;
     if (num % 2 == 1) {
       num = num * 3 + 1;
-    } 
+    }
     else {
       num /= 2;
     }
@@ -30,8 +30,8 @@ unsigned int collatzLen(uint64_t num) {
   return len;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, 
-uint64_t lbound, 
+unsigned int seqCollatz(unsigned int *maxlen,
+uint64_t lbound,
 uint64_t rbound) {
   uint64_t numb = 0;
   *maxlen = 2;
