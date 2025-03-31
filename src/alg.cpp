480fbf8d3a41a2b92a172b11ceac79f6de95a085
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-03-31 18:01:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2025 NNTU-CS
-#include <cstdint>
 #include "alg.h"
+#include <cstdint>
 #include <algorithm>
 
 unsigned int collatzLen(uint64_t num) {
-  unsigned int length = 1; 
-  while(num != 1) {
-    if(num % 2 == 0)
+  unsigned int length = 1;
+  while (num != 1) {
+    if (num % 2 == 0)
       num /= 2;
     else
       num = 3 * num + 1;
@@ -17,12 +17,12 @@ unsigned int collatzLen(uint64_t num) {
 
 uint64_t collatzMaxValue(uint64_t num) {
   uint64_t maxVal = num;
-  while(num != 1) {
-    if(num % 2 == 0)
+  while (num != 1) {
+    if (num % 2 == 0)
       num /= 2;
     else
       num = 3 * num + 1;
-    if(num > maxVal)
+    if (num > maxVal)
       maxVal = num;
   }
   return maxVal;
@@ -31,9 +31,9 @@ uint64_t collatzMaxValue(uint64_t num) {
 unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
   uint64_t bestNumber = lbound;
   *maxlen = 0;
-  for(uint64_t i = lbound; i <= rbound; ++i) {
+  for (uint64_t i = lbound; i <= rbound; ++i) {
     unsigned int currentLen = collatzLen(i);
-    if(currentLen > *maxlen) {
+    if (currentLen > *maxlen) {
       *maxlen = currentLen;
       bestNumber = i;
     }
