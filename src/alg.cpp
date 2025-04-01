8d3ecce9b36bc3c423fc331de71cec27055809a5
notpsnk <frazzle.kibbles_0i@icloud.com>
2025-04-01 12:25:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,15 +8,15 @@ uint64_t collatzMaxValue(uint64_t num) {
     if (num % 2 == 0) {
       num = num / 2;
     } else {
-      num = num * 3 - 1;
+      num = num * 3 + 1;
     }
     count++;
   }
-  return count + 1;
+  return count;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  uint64_t maxV = 0;
+  uint64_t maxV = num;
   while (num != 1) {
     if (num & 1) {
       num = num * 3 + 1;
@@ -34,7 +34,7 @@ unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t rbound) {
   uint64_t best = lbound;
   *maxlen = 1;
-  for (uint64_t val = lbound; val < rbound; ++val) {
+  for (uint64_t val = lbound; val <= rbound; ++val) {
     unsigned int len = collatzLen(val);
     if (len > *maxlen) {
       *maxlen = len;
