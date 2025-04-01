d7f61df4bf80e9fe046c609414461f21bd4bc8c0
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-04-01 08:25:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,71 +3,70 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-   if (num == 0) {
-        return 0;
+  if (num == 0) {
+    return 0;
+  }
+  uint64_t maxVal = num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
     }
-    uint64_t maxVal = num;
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
-        } else {
-            num = 3 * num + 1;
-        }
-        
-        if (num > maxVal) {
-            maxVal = num;
-        }
+    
+    if (num > maxVal) {
+      maxVal = num;
     }
-    return maxVal;
+  }
+  return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
   if (num == 0) {
-        return 0;
+    return 0;
   }
-    unsigned int length = 1;
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
-        } else {
-            num = 3 * num + 1;
-        }
-        ++length;
+  unsigned int length = 1;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
     }
-    return length;
+    ++length;
+  }
+  return length;
 }
 
 unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
+                       uint64_t lbound,
+                       uint64_t rbound) {
   if (maxlen == nullptr || lbound == 0 || rbound == 0 || lbound > rbound) {
-        if (maxlen) *maxlen = 0;
-        return 0;
+    if (maxlen) *maxlen = 0;
+    return 0;
+  }
+
+  unsigned int maxLength = 0;
+  unsigned int resultNum = lbound;
+
+  for (uint64_t num = lbound; num <= rbound; ++num) {
+    uint64_t current = num;
+    unsigned int currentLength = 1;
+
+    while (current != 1) {
+      if (current % 2 == 0) {
+        current /= 2;
+      } else {
+        current = 3 * current + 1;
+      }
+      ++currentLength;
     }
 
-    unsigned int maxLength = 0;
-    unsigned int resultNum = lbound;
-
-    for (uint64_t num = lbound; num <= rbound; ++num) {
-        uint64_t current = num;
-        unsigned int currentLength = 1;  // Само число уже учитывается
-
-        while (current != 1) {
-            if (current % 2 == 0) {
-                current /= 2;
-            } else {
-                current = 3 * current + 1;
-            }
-            ++currentLength;
-        }
-
-        if (currentLength > maxLength) {
-            maxLength = currentLength;
-            resultNum = num;
-        }
+    if (currentLength > maxLength) {
+      maxLength = currentLength;
+      resultNum = num;
     }
+  }
 
-    *maxlen = maxLength;
-    return static_cast<unsigned int>(resultNum);
+  *maxlen = maxLength;
+  return static_cast<unsigned int>(resultNum);
 }
-
