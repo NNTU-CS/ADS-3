d6decc0e2030947994f297760d421cb70f0d47ee
Anitone <133121791+Anitone@users.noreply.github.com>
2025-04-02 14:40:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,10 +5,10 @@
 uint64_t collatzMaxValue(uint64_t num) {
   uint64_t maxValue = num;
   while (num != 1) {
-    if (num % 2 == 0){
+    if (num % 2 == 0) {
       num /= 2;
     } else {
-      num = 3 * num + 1; 
+      num = 3 * num + 1;
     }
     if (num > maxValue) {
       maxValue = num;
@@ -20,17 +20,19 @@ uint64_t collatzMaxValue(uint64_t num) {
 unsigned int collatzLen(uint64_t num) {
   unsigned int length = 1;
   while (num != 1) {
-    if (num % 2 == 0){
+    if (num % 2 == 0) {
       num /= 2;
     } else {
-      num = 3 * num + 1; 
+      num = 3 * num + 1;
     }
     length++;
   }
   return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+unsigned int seqCollatz(unsigned int *maxlen,
+                        uint64_t lbound,
+                        uint64_t rbound) {
   unsigned int maxLength = 1;
   uint64_t maxNum = 0;
   for (unsigned int i = lbound; i <= rbound; i++) {
