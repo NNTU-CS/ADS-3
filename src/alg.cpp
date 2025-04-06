b621c3f1a44a2a42e0fa6189c30d2f1d79786d73
ettennad <a.derlukova@mail.ru>
2025-04-06 05:00:49 +0300

    ads-3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,46 @@
 // Copyright 2025 NNTU-CS
 #include <cstdint>
-#include "alg.h"
-
-uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
-}
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int length = 0;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        length++;
+    }
+    return length + 1;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+uint64_t collatzMaxValue(uint64_t num) {
+    uint64_t maxVal = num;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+    return maxVal;
+}
+
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int longestLength = 0;
+    unsigned int numberWithLongestSeq = lbound;
+
+    for (uint64_t i = lbound; i <= rbound; ++i) {
+        unsigned int currentLength = collatzLen(i);
+        if (currentLength > longestLength) {
+            longestLength = currentLength;
+            numberWithLongestSeq = i;
+        }
+    }
+
+    *maxlen = longestLength;
+    return numberWithLongestSeq;
 }
