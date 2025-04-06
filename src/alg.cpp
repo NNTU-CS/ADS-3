ddce7924206652688cfa3b4dcae41a09c5cedc64
ettennad <a.derlukova@mail.ru>
2025-04-06 05:56:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,11 +5,7 @@ uint64_t collatzMaxValue(uint64_t num) {
     uint64_t max_val = num;
 
     while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2; 
-        } else {
-            num = 3 * num + 1; 
-        }
+        num = (num % 2 == 0) ? num / 2 : (3 * num + 1);
         max_val = (num > max_val) ? num : max_val;
     }
     return max_val;
@@ -17,12 +13,9 @@ uint64_t collatzMaxValue(uint64_t num) {
 
 unsigned int collatzLen(uint64_t num) {
     unsigned int length = 0;
+
     while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
-        } else {
-            num = 3 * num + 1;
-        }
+        num = (num % 2 == 0) ? num / 2 : (3 * num + 1);
         length++;
     }
     return length + 1;
@@ -30,7 +23,7 @@ unsigned int collatzLen(uint64_t num) {
 
 unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
     unsigned int longestLength = 0;
-    unsigned int numberWithLongestSeq = lbound;
+    uint64_t numberWithLongestSeq = lbound;
 
     for (uint64_t i = lbound; i <= rbound; ++i) {
         unsigned int currentLength = collatzLen(i);
@@ -39,7 +32,6 @@ unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) 
             numberWithLongestSeq = i;
         }
     }
-
     *maxlen = longestLength;
     return numberWithLongestSeq;
 }
