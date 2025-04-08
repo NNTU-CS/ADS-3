10ce37864b1e7709dddc634dfd04de13a615e044
pivnik1 <niga.gurunov38@mail.ru>
2025-04-08 17:47:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,65 +2,43 @@
 #include <cstdint>
 #include "alg.h"
 
-uint64_t collatzMaxValue(uint64_t number) {
-    uint64_t maximum = number;
-    uint64_t current = number;
-
-    while (true) {
-        if (current == 1) break;
-
-        if (current > maximum) maximum = current;
-
-        if (current & 1) {
-            current = 3 * current + 1;
-        } else {
-            current = current / 2;
+uint64_t collatzMaxValue(uint64_t num) {
+        uint64_t result = num;
+        while (num != 1) {
+                if (num % 2 == 0)
+                        num = num / 2;
+                else
+                        num = 3 * num + 1;
+                if (num > result)
+                        result = num;
         }
-    }
-    return maximum;
+        return result;
 }
 
-unsigned int collatzLen(uint64_t value) {
-    unsigned int length = 0;
-    uint64_t temp = value;
-
-    do {
-        length++;
-        if (temp & 1) {
-            temp = 3 * temp + 1;
-        } else {
-            temp = temp / 2;
+unsigned int collatzLen(uint64_t num) {
+        unsigned int length = 1;
+        while (num != 1) {
+                if (num % 2 == 0)
+                        num = num / 2;
+                else
+                        num = 3 * num + 1;
+                length++;
         }
-    } while (temp != 1);
-
-    return length;
+        return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
-    unsigned int max_length = 0;
-    uint64_t best_number = lbound;
-
-    uint64_t current_num = lbound;
-    while (current_num <= rbound) {
-        uint64_t temp_value = current_num;
-        unsigned int current_length = 1;
-
-        while (temp_value != 1) {
-            if (temp_value & 1) {
-                temp_value = 3 * temp_value + 1;
-            } else {
-                temp_value = temp_value / 2;
-            }
-            current_length++;
+unsigned int seqCollatz(unsigned int *maxlen,
+                        uint64_t lbound,
+                        uint64_t rbound) {
+        unsigned int maxLength = 1;
+        unsigned int numberWithMaxLength = lbound;
+        for (uint64_t i = lbound; i <= rbound; i++) {
+                unsigned int currentLength = collatzLen(i);
+                if (currentLength > maxLength) {
+                        maxLength = currentLength;
+                        numberWithMaxLength = i;
+                }
         }
-
-        if (current_length > max_length) {
-            max_length = current_length;
-            best_number = current_num;
-        }
-        current_num++;
-    }
-
-    *maxlen = max_length;
-    return best_number;
+        *maxlen = maxLength;
+        return numberWithMaxLength;
 }
