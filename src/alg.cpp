9fa13fa09ff66a2e1b252f2a9a80b4fbc45839b6
Sibilka <barsik05072005@gmail.com>
2025-04-10 20:05:06 +0300

    3 ads

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,55 @@
-// Copyright 2025 NNTU-CS
+// Copyright 2022 NNTU-CS
+#include <cmath>
 #include <cstdint>
 #include "alg.h"
 
-uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+
+
+double pown(double value, uint16_t n) {
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    result *= value;
+  }
+  return result;
 }
 
-unsigned int collatzLen(uint64_t num) {
-  return 1;
+uint64_t fact(uint16_t n) {
+  uint64_t result = 1;
+  for (uint16_t i = 1; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+double calcItem(double x, uint16_t n) {
+  if (n == 0) {
+    return 1.0;
+  }
+  return (pown(x, n) / fact(n));
+}
+
+double expn(double x, uint16_t count) {
+  double result = 1.0;
+    for (uint16_t i = 1; i <= count; ++i) {
+      result += calcItem(x, i);
+    }
+  return result;
+}
+
+double sinn(double x, uint16_t count) {
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    double term = (n % 2 == 0 ? 1 : -1) * pown(x, 2 * n + 1) / fact(2 * n + 1);
+    sum += term;
+    }
+  return sum;
+}
+
+double cosn(double x, uint16_t count) {
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    double term = (n % 2 == 0 ? 1 : -1) * pown(x, 2 * n) / fact(2 * n);
+    sum += term;
+  }
+  return sum;
 }
