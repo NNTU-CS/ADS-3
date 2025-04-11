6c495117fe7e24be5af957ca1bd672af09b2b669
v1nk03 <vovakonoval823@gmail.com>
2025-04-11 23:39:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,16 +2,52 @@
 #include <cstdint>
 #include "alg.h"
 
-uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+uint64_t collatzMaxValue(uint64_t startNumber) {
+    uint64_t maxValue = startNumber;
+    uint64_t currentNumber = startNumber; // Introduce a working copy
+    while (currentNumber != 1) {
+        if (currentNumber % 2 == 0) {
+            currentNumber /= 2;
+        } else {
+            currentNumber = 3 * currentNumber + 1;
+        }
+        if (currentNumber > maxValue) {
+            maxValue = currentNumber;
+        }
+    }
+    return maxValue;
 }
 
-unsigned int collatzLen(uint64_t num) {
-  return 1;
+unsigned int collatzLen(uint64_t startNumber) {
+    unsigned int sequenceLength = 1;
+    uint64_t currentNumber = startNumber; //Introduce a working copy
+
+    while (currentNumber != 1) {
+        if (currentNumber % 2 == 0) {
+            currentNumber /= 2;
+        } else {
+            currentNumber = 3 * currentNumber + 1;
+        }
+        sequenceLength++;
+    }
+    return sequenceLength;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int *maxSequenceLength,
+    uint64_t lowerBound,
+    uint64_t upperBound) {
+    unsigned int maxLength = 1;
+    unsigned int numberAtMaxLength = lowerBound;
+
+    for (uint64_t i = lowerBound; i <= upperBound; ++i) {
+        unsigned int currentLength = collatzLen(i);
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+            numberAtMaxLength = i;
+        }
+    }
+
+    *maxSequenceLength = maxLength; //Use descriptive name
+
+    return numberAtMaxLength;
 }
