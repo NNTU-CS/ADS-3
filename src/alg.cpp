38b1afb4bfa58531333215720b6a9bf1d7a27031
1POMI1 <ptsumr@gmail.com>
2025-04-17 12:38:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,11 +27,11 @@ uint64_t collatzMaxValue(uint64_t num) {
     return maxVal;
 }
 
-unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound, uint64_t rbound) {
+unsigned int seqCollatz(unsigned int* maxlen,
+uint64_t lbound, uint64_t rbound) {
     unsigned int maxLength = 0;
     unsigned int number = 0;
-    for (uint64_t i = lbound;
-    i <= rbound; ++i) {
+    for (uint64_t i = lbound; i <= rbound; ++i) {
         unsigned int len = collatzLen(i);
         if (len > maxLength) {
             maxLength = len;
