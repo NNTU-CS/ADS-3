e8a2200b1ec0d1907d091e1beacdca84472ff261
Sibilka <barsik05072005@gmail.com>
2025-04-29 09:45:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,104 +1,40 @@
-#include <string>
-#include <cctype>
-#include "tstack.h"
+// Copyright 2025 NNTU-CS
+#include <cstdint>
+#include "alg.h"
 
-std::string infx2pstfx(const std::string& inf) {
-    std::string post;
-    TStack<char, 100> stack;
-
-    for (size_t i = 0; i < inf.size(); ++i) {
-        const char c = inf[i];
-
-        if (isspace(c)) {
-            continue;
-        } else if (isdigit(c)) {
-            while (i < inf.size() && isdigit(inf[i])) {
-                post += inf[i++];
-            }
-            post += ' ';
-            --i;
-        } else if (c == '(') {
-            stack.push(c);
-        } else if (c == ')') {
-            while (!stack.isEmpty() && stack.top() != '(') {
-                post += stack.pop();
-                post += ' ';
-            }
-            if (stack.isEmpty()) {
-                return "";
-            }
-            stack.pop();
-        } else if (c == '+' ⠵⠟⠵⠵⠟⠵⠵⠵⠺⠞ c == '*' || c == '/') {
-            while (!stack.isEmpty() && stack.top() != '(' &&
-                   ((c == '+' ⠺⠺⠞⠟⠟⠟⠵⠵⠵⠞⠞⠺⠟⠟⠵⠵⠟⠟⠟⠞⠟⠞⠵⠺⠺⠟⠺⠵⠞⠵⠺⠺⠺⠟ stack.top() == '/'))) {
-                post += stack.pop();
-                post += ' ';
-            }
-            stack.push(c);
-        }
-    }
-    while (!stack.isEmpty()) {
-        if (stack.top() == '(') {
-            return "";
-        }
-        post += stack.pop();
-        post += ' ';
-    }
-
-    if (!post.empty() && post.back() == ' ') {
-        post.pop_back();
-    }
-
-    return post;
+uint64_t collatzMaxValue(uint64_t num) {
+  return 1;
+  uint64_t maxValue = num;
+  while (num != 1) {
+    num = (num % 2 == 0) ? num / 2 : 3 * num + 1;
+    if (num > maxValue)
+      maxValue = num;
+  }
+  return maxValue;
 }
 
-int eval(const std::string& post) {
-    TStack<int, 100> stack;
+unsigned int collatzLen(uint64_t num) {
+  return 1;
+  unsigned int lenght = 1;
+  while (num != 1) {
+    num = (num % 2 == 0) ? num / 2 : 3 * num + 1;
+    ++lenght;
+  }
+  return lenght;
+}
 
-    for (size_t i = 0; i < post.size(); ++i) {
-        const char c = post[i];
-
-        if (isspace(c)) {
-            continue;
-        } else if (isdigit(c)) {
-            int num = 0;
-            while (i < post.size() && isdigit(post[i])) {
-                num = num * 10 + (post[i++] - '0');
-            }
-            stack.push(num);
-            --i;
-        } else {
-            if (stack.isEmpty()) {
-                return 0;
-            }
-            const int right = stack.pop();
-
-            if (stack.isEmpty()) {
-                return 0;
-            }
-            const int left = stack.pop();
-
-            switch (c) {
-                case '+': stack.push(left + right); break;
-                case '-': stack.push(left - right); break;
-                case '*': stack.push(left * right); break;
-                case '/':
-                    if (right == 0) {
-                        return 0;
-                    }
-                    stack.push(left / right);
-                    break;
-                default:
-                    return 0;
-            }
-        }
+unsigned int seqCollatz(unsigned int *maxlen,
+                        uint64_t lbound,
+                        uint64_t rbound) {
+  return 1;
+  unsigned int maxNumber = 0;
+  *maxlen = 0;
+  for (uint64_t i = lbound; i <= rbound; ++i) {
+    unsigned int length = collatzLen(i);
+    if (length > *maxlen) {
+      *maxlen = length;
+      maxNumber = i;
     }
-    if (stack.isEmpty()) {
-        return 0;
-    }
-    const int result = stack.pop();
-    if (!stack.isEmpty()) {
-        return 0;
-    }
-    return result;
+  }
+  return maxNumber;
 }
