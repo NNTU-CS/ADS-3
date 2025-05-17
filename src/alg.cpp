8d7eca22a8ce8797b9554e391b8be46e86eca864
Lina-Mar <martipoli7@gmail.com>
2025-05-17 03:17:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,42 +1,46 @@
 // Copyright 2025 NNTU-CS
-#include <cstdint>
 #include "alg.h"
-#include <stdio.h>
-#include <stdint.h>
-#include <limits.h>
-#include <stdlib.h>
 
 uint64_t collatzMaxValue(uint64_t num) {
-    uint64_t max = num;
-    while (num != 1) {
-        num = (num % 2 == 0) ? num / 2 : (num > (UINT64_MAX-1)/3 ? (printf("Overflow: %lu\n",num), exit(1), 0) : 3*num + 1);
-        if (num > max) max = num;
+  uint64_t maxVal = num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
     }
-    return max;
+    if (num > maxVal) {
+      maxVal = num;
+    }
+  }
+  return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
-    unsigned int len = 1;
-    while (num != 1) {
-        num = (num % 2 == 0) ? num / 2 : (num > (UINT64_MAX-1)/3 ? (printf("Overflow: %lu\n",num), exit(1), 0) : 3*num + 1);
-        len++;
-        if (len > 1000000) { printf("Sequence too long\n"); exit(1); }
+  unsigned int length = 1; // Начальная длина, включая само число
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
     }
-    return len;
+    length++;
+  }
+  return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
-    unsigned int best_len = collatzLen(lbound);
-    uint64_t best_num = lbound;
-    
-    for (uint64_t num = lbound+1; num <= rbound; num++) {
-        unsigned int current = collatzLen(num);
-        if (current > best_len) {
-            best_len = current;
-            best_num = num;
-        }
+unsigned int seqCollatz(unsigned int *maxlen,
+                        uint64_t lbound,
+                        uint64_t rbound) {
+  unsigned int maxLength = 0;
+  unsigned int numberWithMaxLength = lbound;
+  for (uint64_t i = lbound; i <= rbound; ++i) {
+    unsigned int length = collatzLen(i);
+    if (length > maxLength) {
+      maxLength = length;
+      numberWithMaxLength = i;
     }
-    
-    *maxlen = best_len;
-    return best_num;
+  }
+  *maxlen = maxLength;
+  return numberWithMaxLength;
 }
